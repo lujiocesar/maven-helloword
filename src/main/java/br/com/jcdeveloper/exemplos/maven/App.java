2d3f36b22ca8ee7bbf0921826fa497488c6705d6
lujiocesar <lujiocesar@gmail.com>
2022-06-08 13:05:11 -0300

    Initial commit

---
+++ src/main/java/br/com/jcdeveloper/exemplos/maven/App.java
@@ -0,0 +1,13 @@
+package br.com.jcdeveloper.exemplos.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( " Maven - Hello World!" );
+    }
+}
